872800e71af614c8c1aed58f839a203093f55295
Edward Fauchon-Jones <Galadirith@users.noreply.github.com>
2014-09-29 01:02:42 +0100

    maths blocks are terminated by $$ not blank line
    consumeMaths no longer stops consumtion of lines when it finds a blank
    line. It now stops consumption only when the closing delimiter $$ is
    found.

--- MathsMarkdown.php
+++ MathsMarkdown.php
@@ -23,6 +23,18 @@ class MathsMarkdown extends Markdown {
 	
 	/**
 	 * Consume lines for a maths block
+	 * 
+	 * A maths block should be preceded and followed by a blank line (with
+	 * standard exceptions such as being preceded by a header). The maths
+	 * block is delimited by $$ at its beginning and end.
+	 * 
+	 * The opening and closing delimiters $$ should be placed on their own
+	 * lines. Anything placed after the opening $$ on the same line will be
+	 * ignored, leaving only the $$ in the output render. Similarly, anything
+	 * placed before the closing $$ will be ignored.
+	 * 
+	 * The maths block may contain blank lines between the delimiters, and
+	 * they will NOT be interpreted as a new paragraph.
 	 */
 	protected function consumeMaths( $lines, $current ) {
 
@@ -31,15 +43,17 @@ class MathsMarkdown extends Markdown {
 			'content' => [],
 		];
 		
-		// Consume until empty newline (indents are NOT treated as quotes)
-		for( $i = $current, $count = count($lines); $i < $count; $i++ ) {
-			if(	ltrim($lines[$i]) !== '' )
+		// Consume until closing $$ delimiter is found
+		$block['content'][] = '$$';
+		for( $i = $current+1, $count = count($lines); $i < $count; $i++ ) {
+			if( !preg_match( '/\$\$$/', $lines[$i] ) )
 				$block['content'][] = $lines[$i];
 			else
 				break;
 		}
-
-		return [$block, --$i];
+		$block['content'][] = '$$';
+		
+		return [$block, $i];
 	}
 	
 	/**
